deea188439acc3a579b33fd30cefed2c3b26588c
wongmofong0125 <wongmofong@gmail.com>
2026-03-08 00:12:10 -0500

    Fix SQL insert statement formatting

--- serviceapi/app.py
+++ serviceapi/app.py
@@ -115,7 +115,7 @@ def get_signed_url():
             # Prepare the SQL Insert
             insert_stmt = sqlalchemy.text("""
                 INSERT INTO app_user (id, firebase_uid, email) 
-                VALUES (1, 'wongmofong', 'wongmofong.gmail.com'))
+                VALUES (1, 'wongmofong', 'wongmofong.gmail.com')
                
             """)
             #VALUES (1, gen_random_uuid(), credentials, credentials.service_account_email))
